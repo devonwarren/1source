547acedec4e61c62d8dccb6a1896006dbf56ad02
Devon Warren <devon.warren@gmail.com>
2014-08-22 15:02:59 -0400

    add inline admin forms

--- sections/admin.py
+++ sections/admin.py
@@ -2,11 +2,18 @@ from sections import models
 from django.contrib import admin
 from ordered_model.admin import OrderedModelAdmin
 
+class SubSectionInline(admin.StackedInline):
+	model = models.SubSection
+	extra = 0
+
 class SectionAdmin(OrderedModelAdmin):
 	list_display = ('title', 'move_up_down_links')
+	inlines = [
+		SubSectionInline,
+	]
 
 class SubSectionAdmin(OrderedModelAdmin):
-	list_display = ('name', 'move_up_down_links')
+	list_display = ('section', 'name', 'move_up_down_links')
 
 admin.site.register(models.Section, SectionAdmin)
 admin.site.register(models.SubSection, SubSectionAdmin)
