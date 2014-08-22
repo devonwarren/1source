699977a7477261c82aba561b4b9df1b41daa0da3
Devon Warren <devon.warren@gmail.com>
2014-08-22 13:56:17 -0400

    enable content toggling on homepage

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,5 +1,5 @@
 function fullHeightHero() {
-	jQuery('#hero').css('height', window.innerHeight + 'px');
+	$('#hero').css('height', window.innerHeight + 'px');
 }
 
 jQuery(window).load(function() {
@@ -10,6 +10,15 @@ jQuery(window).resize(function() {
 	fullHeightHero();
 });
 
-function homepageDetailLoad() {
-	return true;
+function homepageDetailLoad(section_id, subsection_id) {
+	// change content
+	$('#section-' + section_id).find('div.active').fadeOut(function() {
+		$(this).removeClass('active').parent().find('div#subsection-' + subsection_id).fadeIn().addClass('active');
+	});
+
+	
+
+	// change link active
+	$('#aside-' + section_id).find('li.active').removeClass('active');
+	$('#aside-' + section_id).find('li#subsection-select-' + subsection_id).addClass('active');
 }
