cc7cdbce4f98033e17a814cb97af571066d8190c
Devon Warren <devon.warren@gmail.com>
2014-08-25 16:44:23 -0400

    make section navs active as they are scrolled onto

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -16,9 +16,32 @@ function homepageDetailLoad(section_id, subsection_id) {
 		$(this).removeClass('active').parent().find('div#subsection-' + subsection_id).fadeIn().addClass('active');
 	});
 
-	
-
 	// change link active
 	$('#aside-' + section_id).find('li.active').removeClass('active');
 	$('#aside-' + section_id).find('li#subsection-select-' + subsection_id).addClass('active');
 }
+
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+    var top = $(this).scrollTop();
+    var anchors = $('.anchor');
+    var active = '';
+    anchors.each(function(ind, val) {
+    	var offset = $(val).offset();
+    	if (top >= offset.top && $('#nav-'+val.id).length > 0) {
+    		active = val.id;
+    	}
+    });
+
+	$('nav a').each(function(ind, val) {
+		if (val.id != 'nav-'+active) {
+			$(val).removeClass('active');
+		} else {
+			$(val).addClass('active');
+		}
+	});
+
+  });
+
+});
