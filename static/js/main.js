3920f6719c3b2c47eeedfe35f915f45c04d41a26
Devon Warren <devon.warren@gmail.com>
2014-09-03 15:02:06 -0400

    center mobile menu logo

--- static/js/main.js
+++ static/js/main.js
@@ -56,6 +56,31 @@ function toggleMobileMenu() {
   $('ul#fullmenu').toggleClass('open');
 }
 
+function fullHeightHero() {
+  $('#hero').css('height', window.innerHeight + 'px');
+}
+
+$(window).load(function() {
+  fullHeightHero();
+});
+
+$(window).resize(function() { 
+  fullHeightHero();
+});
+
+function homepageDetailLoad(subsection) {
+  var section = $('#subsection-' + subsection).parent().parent();
+  // change content
+
+  $(section).find('div.active').fadeOut(function() {
+    $(this).removeClass('active').parent().find('div#subsection-' + subsection).fadeIn().addClass('active');
+  });
+
+  // change link active
+  $(section).find('li.active').removeClass('active');
+  $(section).find('li#subsection-select-' + subsection).addClass('active');
+}
+
 $(document).ready(function() {
 
   $(window).scroll(function() {
@@ -67,5 +92,20 @@ $(document).ready(function() {
       toggleMobileMenu();
     }
   });
+
+  if ($('body').hasClass('homepage')) {
+    if (window.location.hash) {
+      homepageDetailLoad(window.location.hash.replace('#subsec-',''));
+    }
+
+    $('ul.subsection-links a').click(function() {
+      idx = this.href.indexOf('#');
+      if (idx != -1) {
+        homepageDetailLoad(this.href.substring(idx).replace('#subsec-',''));  
+      }
+    });
+  }
+
+  scrollEvent();
 
 });
