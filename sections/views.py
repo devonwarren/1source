c08795631adb25c533ee3b79ba8003f536ffe408
Devon Warren <devon.warren@gmail.com>
2014-09-10 11:28:57 -0400

    learn more pages should use name not title

--- sections/views.py
+++ sections/views.py
@@ -6,7 +6,9 @@ from django.http import HttpResponse
 
 def learn_more_view(request, subsection):
 	subsec = get_object_or_404(SubSection, slug=subsection)
+	subsec.title  = subsec.name
 	subsec.content  = subsec.learn_more
+
 
 	t = get_template('page.html')
 	html = t.render(Context({ 'page' : subsec, 'bgimage' : subsec.section.image_web.url }))
