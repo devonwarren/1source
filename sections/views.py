a0de604f1920b079c34756484e75b06e68e60115
Devon Warren <devon.warren@gmail.com>
2014-09-11 14:35:56 -0400

    add options to navigate back from learn more pages

--- sections/views.py
+++ sections/views.py
@@ -6,10 +6,10 @@ from django.http import HttpResponse
 
 def learn_more_view(request, subsection):
 	subsec = get_object_or_404(SubSection, slug=subsection)
-	subsec.title  = subsec.name
+	subsec.title  = '<a href="/#' + subsec.section.slug + '">' + subsec.section.title + '</a> : ' + subsec.name
 	subsec.content  = subsec.learn_more
 
 
 	t = get_template('page.html')
-	html = t.render(Context({ 'page' : subsec, 'bgimage' : subsec.section.image_web.url }))
+	html = t.render(Context({ 'page' : subsec, 'bgimage' : subsec.section.image_web.url, 'active_nav' : subsec.section.slug }))
 	return HttpResponse(html)
