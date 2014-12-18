9d77fc1ee402045bb26cbfc8bd48bb3ce47d92e8
Devon Warren <devon.warren@gmail.com>
2014-12-18 14:56:52 -0500

    actually remove the debug toolbar

--- onesource/settings.py
+++ onesource/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.formtools',
+#    'debug_toolbar',
     'ordered_model',
     'autoslug',
     'ckeditor',
@@ -59,7 +60,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'onesource.urls'
